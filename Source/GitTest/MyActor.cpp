26c0d7e0f2c461fa304b21d21a8ddc1a1bc77faa
TsukinoRen <whitealbum1226@gmail.com>
2026-04-02 01:11:48 +0900

    [Mine] MyActor 클래스 수정

--- Source/GitTest/MyActor.cpp
+++ Source/GitTest/MyActor.cpp
@@ -16,6 +16,7 @@ void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
 }
 
 // Called every frame
@@ -23,5 +24,6 @@ void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
 }
 
