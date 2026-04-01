2b4099c61fb25c15d3e5d5cbc1939241bbf47695
TsukinoRen <whitealbum1226@gmail.com>
2026-04-02 01:17:45 +0900

    [Other] MyActor 클래스 리팩토링

--- Source/GitTest/MyActor.cpp
+++ Source/GitTest/MyActor.cpp
@@ -16,7 +16,7 @@ void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
+	//UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
 }
 
 // Called every frame
@@ -24,6 +24,6 @@ void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
+	//UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
 }
 
